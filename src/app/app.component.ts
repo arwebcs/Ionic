3937bd2d7ce2498af77d678e90b1fa67186aa33b
arwebcs <arwebcs@gmail.com>
2023-11-19 11:12:07 +0530

    Back button fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,30 +9,17 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  //code for exit app
-  // set up hardware back button event.
-  lastTimeBackPress = 0;
-  timePeriodToExit = 2000;
-
   constructor(
     private platform: Platform,
     public alertController: AlertController,
     private router: Router,
     private _location: Location
   ) {
-    // Initialize BackButton Eevent.
-    //this.backButtonEvent();
-
     this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
-      // console.log('Back press handler!');
       if (this._location.isCurrentPathEqualTo('/home')) {
-        // Show Exit Alert!
-        // console.log('Show Exit Alert!');
         this.showExitConfirm();
-        processNextHandler();
+        // processNextHandler();
       } else {
-        // Navigate to back page
-        // console.log('Navigate to back page');
         this._location.back();
       }
     });
